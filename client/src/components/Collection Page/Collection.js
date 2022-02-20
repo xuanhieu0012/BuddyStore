9f7a47ffa6c9236a79d78855b12d12f6393c670e
Hieu <xuanhieu0012@gmail.com>
2022-02-19 23:32:22 -0600

    add active navbar, done

--- client/src/components/Collection Page/Collection.js
+++ client/src/components/Collection Page/Collection.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from '../NavBar/Header'
 import CollectionContent from './CollectionContent';
+import Testimonial from '../Testtimonial/Testimonial'
+import Footer from '../Footer/Footer'
 function Collection(){
 
     return <div>
         <Header />
         <CollectionContent />
+        
+        <Testimonial />
+        <Footer />
     </div>
 }
 
--- client/src/components/NavBar/Header.js
+++ client/src/components/NavBar/Header.js
@@ -1,13 +1,20 @@
 import React,{useState} from 'react';
 import logo from "../../assets/logo.png"
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {VscAccount} from "react-icons/vsc"
 import {BsSearch} from "react-icons/bs"
 import {RiShoppingBag3Line} from "react-icons/ri"
 import {GiHamburgerMenu} from 'react-icons/gi'
 import './Style.css'
 function Header(){
-   
+    const location = useLocation();
+
+    //destructuring pathname from location
+    const { pathname } = location;
+
+    //Javascript split method to get the name of the path in array
+    const splitLocation = pathname.split("/");
+    console.log(splitLocation)
     function isResponsive() {
         let x = document.getElementById("link-container");
         if (x.className === "header-link-content") {
@@ -28,12 +35,12 @@ function Header(){
                 </div>
                     <div className="header-link-content" id='link-container'>
                         <div className="nav-link-container">
-                            <Link to='/bestSeller'>Best Seller</Link>
-                            <Link to='/collection'>Collection</Link>
-                            <Link to='/shop'>Shop</Link>
-                            <Link to='/sales'>Sales</Link>
-                            <Link to='/story'>Story</Link>
-                            <Link to='/contactUs'>Contact Us</Link>
+                            <Link to='/bestSeller' className={splitLocation[1] === 'bestSeller' ? 'active' : ''}>Best Seller</Link>
+                            <Link to='/collection' className={splitLocation[1] === 'collection' ? 'active' : ''}>Collection</Link>
+                            <Link to='/shop'className={splitLocation[1] === 'shop' ? 'active' : ''}>Shop</Link>
+                            <Link to='/sales' className={splitLocation[1] === 'sales' ? 'active' : ''}>Sales</Link>
+                            <Link to='/story' className={splitLocation[1] === 'story' ? 'active' : ''}>Story</Link>
+                            <Link to='/contactUs' className={splitLocation[1] === 'contactUs' ? 'active' : ''}>Contact Us</Link>
                         </div>
                         <div className='user-header-content'>
                             <Link to='/account'><VscAccount size='30px'/></Link>
